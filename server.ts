b96cd5bee5d6b12f9da550472b52962d2e33dd7f
Dimka863 <dlysachenko98@gmail.com>
2024-08-02 10:41:58 +0200

    Structure of the project created ts configured

---
+++ server.ts
@@ -0,0 +1,12 @@
+// Importing module
+const express = require('express');
+
+const app = express();
+const PORT: number = 3000;
+
+// Server setup
+app.listen(PORT, () => {
+  console.log(
+    'The application is listening ' + 'on port http://localhost:' + PORT
+  );
+});
