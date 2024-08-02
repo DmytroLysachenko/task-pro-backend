b63619e27055b947a018afe6816314d3ca9856ec
Dimka863 <dlysachenko98@gmail.com>
2024-08-02 19:46:41 +0200

    done

--- dist/helpers/env.js
+++
@@ -1,8 +0,0 @@
-export const env = (name, defaultValue) => {
-    const value = process.env[name];
-    if (value)
-        return value;
-    if (defaultValue)
-        return defaultValue;
-    throw new Error(`No value in process.env[${name}]`);
-};
